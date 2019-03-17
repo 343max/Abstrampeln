5fb8682a83577a03b4cb43b8e0dde8c0d6b83e81
Max von Webel <max@343max.de>
2019-03-17 21:53:59 +0100

    minor cleanup in the `RouteViewController`

--- Abstrampeln/RouteView/RouteViewController.swift
+++ Abstrampeln/RouteView/RouteViewController.swift
@@ -2,17 +2,6 @@
 
 import UIKit
 
-enum DestinationSelectionPurpose {
-  case Display
-  case Preview
-  case Navigation
-}
-
-protocol SelectedDestinationListener {
-  func didSelect(destination: Location, for: DestinationSelectionPurpose)
-  func didDeselectDestination()
-}
-
 class RouteViewController: DrawerViewController {
   let destination: Location
   let signalDispatcher = AppController.shared.dispatcher
@@ -33,19 +22,7 @@ class RouteViewController: DrawerViewController {
     destinationLabel.text = "→ \(destination.label)"
   }
   
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    
-    signalDispatcher.each(SelectedDestinationListener.self) { $0.didSelect(destination: destination, for: .Display) }
-  }
-  
-  override func viewWillDisappear(_ animated: Bool) {
-    super.viewDidDisappear(animated)
-    
-    signalDispatcher.each(SelectedDestinationListener.self) { $0.didDeselectDestination() }
-  }
-  
   @IBAction func tappedPreviewRoute(_ sender: Any) {
-    signalDispatcher.each(SelectedDestinationListener.self) { $0.didSelect(destination: destination, for: .Preview) }
+    //
   }
 }
