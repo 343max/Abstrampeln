08fc55fef0ad6458e5f2757c723c0ecc005e5da5
Max von Webel <max@343max.de>
2019-04-08 12:17:40 +0200

    Added `Promise` to it’s own pod

---
+++ Libs/Promise/Example/Pods/Target Support Files/Pods-Promise_Tests/Pods-Promise_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Promise_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Promise_Tests
+@end
---
+++ Libs/Promise/Example/Pods/Target Support Files/Promise/Promise-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Promise : NSObject
+@end
+@implementation PodsDummy_Promise
+@end
